c4207bef740ff58acb5145239ebe7193e92206ad
ousttrue <oustrrue@gmail.com>
2018-01-13 15:21:10 +0900

    refactoring

--- Assets/UniGLTF/Editor/glTFImporter.cs
+++ Assets/UniGLTF/Editor/glTFImporter.cs
@@ -180,6 +180,99 @@ namespace UniGLTF
             return create();
         }
 
+        static void ReadAnimation(AnimationClip clip, Animation[] animations, List<TransformWithSkin> nodes, GltfBuffer buffer)
+        {
+            foreach (var x in animations)
+            {
+                foreach (var y in x.channels)
+                {
+                    var node = nodes[y.target.node];
+                    switch (y.target.path)
+                    {
+                        case "translation":
+                            {
+                                var curveX = new AnimationCurve();
+                                var curveY = new AnimationCurve();
+                                var curveZ = new AnimationCurve();
+
+                                var sampler = x.samplers[y.sampler];
+                                var input = buffer.GetBuffer<float>(sampler.input);
+                                var output = buffer.GetBuffer<Vector3>(sampler.output);
+                                for (int i = 0; i < input.Length; ++i)
+                                {
+                                    var time = input[i];
+                                    var pos = output[i].ReverseZ();
+                                    curveX.AddKey(time, pos.x);
+                                    curveY.AddKey(time, pos.y);
+                                    curveZ.AddKey(time, pos.z);
+                                }
+
+                                var relativePath = node.Transform.RelativePathFrom(nodes[0].Transform);
+                                clip.SetCurve(relativePath, typeof(Transform), "localPosition.x", curveX);
+                                clip.SetCurve(relativePath, typeof(Transform), "localPosition.y", curveY);
+                                clip.SetCurve(relativePath, typeof(Transform), "localPosition.z", curveZ);
+                            }
+                            break;
+
+                        case "rotation":
+                            {
+                                var curveX = new AnimationCurve();
+                                var curveY = new AnimationCurve();
+                                var curveZ = new AnimationCurve();
+                                var curveW = new AnimationCurve();
+
+                                var sampler = x.samplers[y.sampler];
+                                var input = buffer.GetBuffer<float>(sampler.input);
+                                var output = buffer.GetBuffer<Quaternion>(sampler.output);
+                                for (int i = 0; i < input.Length; ++i)
+                                {
+                                    var time = input[i];
+                                    var rot = output[i].ReverseZ();
+                                    curveX.AddKey(time, rot.x);
+                                    curveY.AddKey(time, rot.y);
+                                    curveZ.AddKey(time, rot.z);
+                                    curveW.AddKey(time, rot.w);
+                                }
+
+                                var relativePath = node.Transform.RelativePathFrom(nodes[0].Transform);
+                                clip.SetCurve(relativePath, typeof(Transform), "localRotation.x", curveX);
+                                clip.SetCurve(relativePath, typeof(Transform), "localRotation.y", curveY);
+                                clip.SetCurve(relativePath, typeof(Transform), "localRotation.z", curveZ);
+                                clip.SetCurve(relativePath, typeof(Transform), "localRotation.w", curveW);
+                            }
+                            break;
+
+                        case "scale":
+                            {
+                                var curveX = new AnimationCurve();
+                                var curveY = new AnimationCurve();
+                                var curveZ = new AnimationCurve();
+
+                                var sampler = x.samplers[y.sampler];
+                                var input = buffer.GetBuffer<float>(sampler.input);
+                                var output = buffer.GetBuffer<Vector3>(sampler.output);
+                                for (int i = 0; i < input.Length; ++i)
+                                {
+                                    var time = input[i];
+                                    var scale = output[i];
+                                    curveX.AddKey(time, scale.x);
+                                    curveY.AddKey(time, scale.y);
+                                    curveZ.AddKey(time, scale.z);
+                                }
+
+                                var relativePath = node.Transform.RelativePathFrom(nodes[0].Transform);
+                                clip.SetCurve(relativePath, typeof(Transform), "localScale.x", curveX);
+                                clip.SetCurve(relativePath, typeof(Transform), "localScale.y", curveY);
+                                clip.SetCurve(relativePath, typeof(Transform), "localScale.z", curveZ);
+                            }
+                            break;
+                    }
+                }
+
+                nodes[0].Transform.gameObject.AddComponent<Animator>();
+            }
+        }
+
         public override void OnImportAsset(AssetImportContext ctx)
         {
             Debug.LogFormat("## GLTFImporter ##: {0}", ctx.assetPath);
@@ -301,95 +394,8 @@ namespace UniGLTF
                 clip.name = ANIMATION_NAME;
                 clip.ClearCurves();
 
-                foreach (var x in animations)
-                {
-                    foreach (var y in x.channels)
-                    {
-                        var node = nodes[y.target.node];
-                        switch (y.target.path)
-                        {
-                            case "translation":
-                                {
-                                    var curveX = new AnimationCurve();
-                                    var curveY = new AnimationCurve();
-                                    var curveZ = new AnimationCurve();
+                ReadAnimation(clip, animations, nodes, buffer);
 
-                                    var sampler = x.samplers[y.sampler];
-                                    var input = buffer.GetBuffer<float>(sampler.input);
-                                    var output = buffer.GetBuffer<Vector3>(sampler.output);
-                                    for (int i = 0; i < input.Length; ++i)
-                                    {
-                                        var time = input[i];
-                                        var pos = output[i].ReverseZ();
-                                        curveX.AddKey(time, pos.x);
-                                        curveY.AddKey(time, pos.y);
-                                        curveZ.AddKey(time, pos.z);
-                                    }
-
-                                    var relativePath = node.Transform.RelativePathFrom(nodes[0].Transform);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localPosition.x", curveX);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localPosition.y", curveY);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localPosition.z", curveZ);
-                                }
-                                break;
-
-                            case "rotation":
-                                {
-                                    var curveX = new AnimationCurve();
-                                    var curveY = new AnimationCurve();
-                                    var curveZ = new AnimationCurve();
-                                    var curveW = new AnimationCurve();
-
-                                    var sampler = x.samplers[y.sampler];
-                                    var input = buffer.GetBuffer<float>(sampler.input);
-                                    var output = buffer.GetBuffer<Quaternion>(sampler.output);
-                                    for (int i = 0; i < input.Length; ++i)
-                                    {
-                                        var time = input[i];
-                                        var rot = output[i].ReverseZ();
-                                        curveX.AddKey(time, rot.x);
-                                        curveY.AddKey(time, rot.y);
-                                        curveZ.AddKey(time, rot.z);
-                                        curveW.AddKey(time, rot.w);
-                                    }
-
-                                    var relativePath = node.Transform.RelativePathFrom(nodes[0].Transform);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localRotation.x", curveX);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localRotation.y", curveY);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localRotation.z", curveZ);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localRotation.w", curveW);
-                                }
-                                break;
-
-                            case "scale":
-                                {
-                                    var curveX = new AnimationCurve();
-                                    var curveY = new AnimationCurve();
-                                    var curveZ = new AnimationCurve();
-
-                                    var sampler = x.samplers[y.sampler];
-                                    var input = buffer.GetBuffer<float>(sampler.input);
-                                    var output = buffer.GetBuffer<Vector3>(sampler.output);
-                                    for (int i = 0; i < input.Length; ++i)
-                                    {
-                                        var time = input[i];
-                                        var scale = output[i];
-                                        curveX.AddKey(time, scale.x);
-                                        curveY.AddKey(time, scale.y);
-                                        curveZ.AddKey(time, scale.z);
-                                    }
-
-                                    var relativePath = node.Transform.RelativePathFrom(nodes[0].Transform);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localScale.x", curveX);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localScale.y", curveY);
-                                    clip.SetCurve(relativePath, typeof(Transform), "localScale.z", curveZ);
-                                }
-                                break;
-                        }
-                    }
-
-                    nodes[0].Transform.gameObject.AddComponent<Animator>();
-                }
                 ctx.AddObjectToAsset(ANIMATION_NAME, clip);
             }
 
