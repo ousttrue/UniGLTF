038b9900be72da4af4aced100e1f7994fcad8f69
ousttrue <ousttrue@gmail.com>
2018-05-30 16:14:07 +0900

    Version 1.7

--- Core/Scripts/UniGLTFVersion.cs
+++ Core/Scripts/UniGLTFVersion.cs
@@ -4,11 +4,11 @@ namespace UniGLTF
     public static class UniGLTFVersion
     {
         public const int MAJOR = 1;
-        public const int MINOR = 6;
+        public const int MINOR = 7;
 
-        public const string VERSION = "1.6";
+        public const string VERSION = "1.7";
 
-        public const string DecrementMenuName = "UniGLTF/Version(1.6) Decrement";
-        public const string IncrementMenuName = "UniGLTF/Version(1.6) Increment";
+        public const string DecrementMenuName = "UniGLTF/Version(1.7) Decrement";
+        public const string IncrementMenuName = "UniGLTF/Version(1.7) Increment";
     }
 }
