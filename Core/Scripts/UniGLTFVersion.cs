d4b94b7690384f3760383121e16328ba1a474646
ousttrue <ousttrue@gmail.com>
2018-06-08 02:11:08 +0900

    Increment version

--- Core/Scripts/UniGLTFVersion.cs
+++ Core/Scripts/UniGLTFVersion.cs
@@ -4,11 +4,11 @@ namespace UniGLTF
     public static class UniGLTFVersion
     {
         public const int MAJOR = 1;
-        public const int MINOR = 8;
+        public const int MINOR = 9;
 
-        public const string VERSION = "1.8";
+        public const string VERSION = "1.9";
 
-        public const string DecrementMenuName = "UniGLTF/Version(1.8) Decrement";
-        public const string IncrementMenuName = "UniGLTF/Version(1.8) Increment";
+        public const string DecrementMenuName = "UniGLTF/Version(1.9) Decrement";
+        public const string IncrementMenuName = "UniGLTF/Version(1.9) Increment";
     }
 }
