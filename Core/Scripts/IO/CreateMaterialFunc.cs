4ca29829407c451ff000b1ac6c00e0232b26ce01
ousttrue <ousttrue@gmail.com>
2018-06-18 22:14:43 +0900

    Separated CreateMaterialFunc and IStorage

---
+++ Core/Scripts/IO/CreateMaterialFunc.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+
+namespace UniGLTF
+{
+    public interface IShaderStore
+    {
+        Shader GetShader(ImporterContext context, int materialIndex);
+    }
+
+    class ShaderStore : IShaderStore
+    {
+        string m_defaultShaderName;
+        Shader m_default;
+        Shader Default
+        {
+            get
+            {
+                if (m_default == null)
+                {
+                    m_default = Shader.Find(m_defaultShaderName);
+                }
+                return m_default;
+            }
+        }
+
+        Shader m_vcolor;
+        Shader VColor
+        {
+            get
+            {
+                if (m_vcolor == null)
+                {
+                    m_vcolor = Shader.Find("UniGLTF/StandardVColor");
+                }
+                return m_vcolor;
+            }
+        }
+
+        public ShaderStore() : this("Standard")
+        {
+
+        }
+
+        public ShaderStore(string defaultShaderName)
+        {
+            m_defaultShaderName = defaultShaderName;
+        }
+
+        public Shader GetShader(ImporterContext context, int materialIndex)
+        {
+            if (context.HasVertexColor(materialIndex))
+            {
+                return VColor;
+            }
+
+            return Default;
+        }
+    }
+
+    public delegate Material CreateMaterialFunc(ImporterContext ctx, int i);
+}
---
+++ Core/Scripts/IO/IStorage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+
+namespace UniGLTF
+{
+    public interface IStorage
+    {
+        ArraySegment<Byte> Get(string url);
+    }
+
+    public class SimpleStorage : IStorage
+    {
+        ArraySegment<Byte> m_bytes;
+
+        public SimpleStorage(ArraySegment<Byte> bytes)
+        {
+            m_bytes = bytes;
+        }
+
+        public ArraySegment<byte> Get(string url)
+        {
+            return m_bytes;
+        }
+    }
+
+    public class FileSystemStorage : IStorage
+    {
+        string m_root;
+
+        public FileSystemStorage(string root)
+        {
+            m_root = Path.GetFullPath(root);
+        }
+
+        public ArraySegment<byte> Get(string url)
+        {
+            var bytes =
+                (url.StartsWith("data:"))
+                ? UriByteBuffer.ReadEmbeded(url)
+                : File.ReadAllBytes(Path.Combine(m_root, url))
+                ;
+            return new ArraySegment<byte>(bytes);
+        }
+    }
+}
