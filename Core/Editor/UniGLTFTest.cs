ddfd218b4e2a859c878152919269ec2bddcf2619
ousttrue <ousttrue@gmail.com>
2018-07-21 09:26:17 +0900

    UniGLTFTest to UniGLTF

--- Core/Editor/UniGLTFTest.cs
+++ Core/Editor/UniGLTFTest.cs
@@ -2,11 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using UniGLTF;
 using UnityEngine;
 
 
-namespace UniGLTFTest
+namespace UniGLTF
 {
     public class Tests
     {
