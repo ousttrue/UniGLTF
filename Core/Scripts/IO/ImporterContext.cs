1adb47fe4264f6b0d691a4eb51857129734f637c
ousttrue <ousttrue@gmail.com>
2018-08-09 14:25:46 +0900

    Added ImporterContext.MeasureTime

--- Core/Scripts/IO/ImporterContext.cs
+++ Core/Scripts/IO/ImporterContext.cs
@@ -11,8 +11,57 @@ using UnityEditor;
 
 namespace UniGLTF
 {
+    public struct KeyElapsed
+    {
+        public string Key;
+        public TimeSpan Elapsed;
+        public KeyElapsed(string key, TimeSpan elapsed)
+        {
+            Key = key;
+            Elapsed = elapsed;
+        }
+    }
+
+    public struct MeasureScope : IDisposable
+    {
+        Action m_onDispose;
+        public MeasureScope(Action onDispose)
+        {
+            m_onDispose = onDispose;
+        }
+        public void Dispose()
+        {
+            m_onDispose();
+        }
+    }
+
     public class ImporterContext
     {
+        public List<KeyElapsed> m_speedReports = new List<KeyElapsed>();
+        public IDisposable MeasureTime(string key)
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            return new MeasureScope(() =>
+            {
+                m_speedReports.Add(new KeyElapsed(key, sw.Elapsed));
+            });
+        }
+        public string GetSpeedLog()
+        {
+            var total = TimeSpan.Zero;
+
+            var sb = new StringBuilder();
+            sb.Append("【SpeedLog】");
+            foreach(var kv in m_speedReports)
+            {
+                sb.AppendLine(string.Format("{0}: {1}ms", kv.Key, (int)kv.Elapsed.TotalMilliseconds));
+                total += kv.Elapsed;
+            }
+            sb.AppendLine(string.Format("total: {0}ms", (int)total.TotalMilliseconds));
+
+            return sb.ToString();
+        }
+
         public UnityPath TextureBaseDir
         {
             get; private set;
