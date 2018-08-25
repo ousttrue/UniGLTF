600a67e012e677cdb691c48e9912d865cda0fe3a
ousttrue <ousttrue@gmail.com>
2018-08-25 15:01:43 +0900

    Increment version to 1.16

--- Core/Scripts/UniGLTFVersion.cs
+++ Core/Scripts/UniGLTFVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class UniGLTFVersion
     {
         public const int MAJOR = 1;
-        public const int MINOR = 15;
+        public const int MINOR = 16;
 
-        public const string VERSION = "1.15";
+        public const string VERSION = "1.16";
     }
 }
