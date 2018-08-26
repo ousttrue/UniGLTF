4c07f03b928b8d7b7f31974ba562cd903c717739
ousttrue <ousttrue@gmail.com>
2018-08-26 13:35:42 +0900

    Incremented version to 1.17

--- Core/Scripts/UniGLTFVersion.cs
+++ Core/Scripts/UniGLTFVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class UniGLTFVersion
     {
         public const int MAJOR = 1;
-        public const int MINOR = 16;
+        public const int MINOR = 17;
 
-        public const string VERSION = "1.16";
+        public const string VERSION = "1.17";
     }
 }
