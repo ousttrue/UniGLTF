732ee43991e9d4b74d6967b7f195c130cebafbd0
ousttrue <ousttrue@gmail.com>
2018-08-30 21:45:33 +0900

    WIP

---
+++ UniUnlit/Editor/UniUnlitEditor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+namespace UniGLTF
+{
+    public class UniUnlitEditor : ShaderGUI
+    {
+        public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+        {
+            base.OnGUI(materialEditor, properties);
+        }
+    }
+}
