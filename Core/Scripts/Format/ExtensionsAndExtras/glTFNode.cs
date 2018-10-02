1f5c39ac527d8250af2b9d8c05eacdd33a77388f
ousttrue <ousttrue@gmail.com>
2018-10-02 18:37:26 +0900

    Added glTFNode_extensions

--- Core/Scripts/Format/ExtensionsAndExtras/glTFNode.cs
+++ Core/Scripts/Format/ExtensionsAndExtras/glTFNode.cs
@@ -4,5 +4,8 @@
 namespace UniGLTF
 {
     [Serializable]
+    public partial class glTFNode_extensions : ExtensionsBase<glTFNode_extensions> { }
+
+    [Serializable]
     public partial class glTFNode_extra : ExtraBase<glTFNode_extra> { }
 }
