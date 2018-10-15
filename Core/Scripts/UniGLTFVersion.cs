55029bbd45c25708370d768b5f5189f53814d124
ousttrue <ousttrue@gmail.com>
2018-10-15 15:30:39 +0900

    Updated version to 1.25

--- Core/Scripts/UniGLTFVersion.cs
+++ Core/Scripts/UniGLTFVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class UniGLTFVersion
     {
         public const int MAJOR = 1;
-        public const int MINOR = 24;
+        public const int MINOR = 25;
 
-        public const string VERSION = "1.24";
+        public const string VERSION = "1.25";
     }
 }
