046b8dc110f6eb55d8ef8d4470d1e7de934261c1
ousttrue <ousttrue@gmail.com>
2018-11-14 16:13:19 +0900

    Version incremented to 1.27

--- Core/Scripts/UniGLTFVersion.cs
+++ Core/Scripts/UniGLTFVersion.cs
@@ -4,8 +4,8 @@ namespace UniGLTF
     public static partial class UniGLTFVersion
     {
         public const int MAJOR = 1;
-        public const int MINOR = 26;
+        public const int MINOR = 27;
 
-        public const string VERSION = "1.26";
+        public const string VERSION = "1.27";
     }
 }
