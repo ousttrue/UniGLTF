7b352bf3b4406f28ecc3eea91bb14935ecd4bcc3
ousttrue <ousttrue@gmail.com>
2018-11-29 16:55:47 +0900

    Fixed GLTFValue

--- Core/Scripts/Format/GLTFJsonFormatter.cs
+++ Core/Scripts/Format/GLTFJsonFormatter.cs
@@ -23,26 +23,36 @@ namespace UniGLTF
             EndList();
         }
 
+        public void GLTFValue(List<string> values)
+        {
+            BeginList();
+            foreach (var value in values)
+            {
+                Value(value);
+            }
+            EndList();
+        }
+
         protected override System.Reflection.MethodInfo GetMethod<T>(Expression<Func<T>> expression)
         {
             var t = typeof(T);
             var formatterType = GetType();
 
-            // try IEnumerable<T>
-            var generic_method = formatterType.GetMethods().First(x => x.Name == "GLTFValue" && x.IsGenericMethod);
-            var ga = t.GetGenericArguments();
-            if (ga.Length > 0)
             {
-                var g = ga[0];
-                var method = generic_method.MakeGenericMethod(g);
+                var method = formatterType.GetMethod("GLTFValue", new Type[] { typeof(T) });
                 if (method != null)
                 {
                     return method;
                 }
             }
 
+            // try IEnumerable<T>
+            var generic_method = formatterType.GetMethods().First(x => x.Name == "GLTFValue" && x.IsGenericMethod);
+            var ga = t.GetGenericArguments();
+            if (ga.Length == 1)
             {
-                var method = formatterType.GetMethod("GLTFValue", new Type[] { typeof(T) });
+                var g = ga[0];
+                var method = generic_method.MakeGenericMethod(g);
                 if (method != null)
                 {
                     return method;
